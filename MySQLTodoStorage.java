94f7873e8088fb461bbc66b8713f2f106d43beea
Grimmjow29 <148979232+Grimmjow29@users.noreply.github.com>
2025-01-02 10:13:34 +0530

    Update MySQLTodoStorage.java

--- MySQLTodoStorage.java
+++ MySQLTodoStorage.java
@@ -10,9 +10,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MySQLTodoStorage implements TodoStorage {
-    private static final String URL = "jdbc:mysql://localhost:3306/todo_management";
+    private static final String URL = "jdbc:mysql://localhost:3306/todo_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "Il1kemysql";
 
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
